32b3e1baabe378b1a1ef735dfb0bab314fafaecb
Ankit <salian.ankit@tcs.com>
2020-04-23 06:25:07 +0530

    Configuring MapStruct and adding test case for the same

---
+++ src/main/java/com/ankit/spring5mvcrest/mvcrest/api/v1/mapper/CategoryMapper.java
@@ -0,0 +1,14 @@
+package com.ankit.spring5mvcrest.mvcrest.api.v1.mapper;
+
+import com.ankit.spring5mvcrest.mvcrest.api.v1.model.CategoryDTO;
+import com.ankit.spring5mvcrest.mvcrest.domain.Category;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
+public interface CategoryMapper {
+
+    CategoryMapper INSTANCE = Mappers.getMapper(CategoryMapper.class);
+
+    CategoryDTO categoryToCategoryDTO(Category category);
+}
